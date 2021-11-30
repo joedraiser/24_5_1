6a0421fc87640e92fce49ce537e6457b63595141
Evgenii.Korchagov <joedraiser@gmail.com>
2021-11-30 09:59:26 +0300

    Amendments done

--- main.cpp
+++ main.cpp
@@ -9,6 +9,37 @@ struct task
     time_t duration;
 };
 
+void begin(std::vector<task>& tasks)
+{
+    std::string input;
+    if(!tasks.empty()&&tasks.back().duration==0)
+        tasks.back().duration=(time_t)(std::difftime(std::time(nullptr), tasks.back().start));
+
+    std::cout << "Type task name: ";
+    std::cin >> input;
+    tasks.push_back({input, std::time(nullptr)});
+}
+
+void end(std::vector<task>& tasks)
+{
+    if(!tasks.empty()&&tasks.back().duration==0)
+        tasks.back().duration=(time_t)(std::difftime(std::time(nullptr), tasks.back().start));
+}
+
+void status(std::vector<task>& tasks)
+{
+    for(int i=0;i<tasks.size();i++)
+    {
+        std::cout << tasks[i].name << ": ";
+        if(!tasks[i].duration)
+        {
+            std::cout << (time_t)(std::difftime(std::time(nullptr), tasks.back().start)) << std::endl;
+        }
+        else
+            std::cout << tasks[i].duration << std::endl;
+    }
+}
+
 int main()
 {
     std::vector<task> tasks;
@@ -19,35 +50,16 @@ int main()
         std::cin >> input;
 
         if(input == "begin")
-        {
-            if(!tasks.empty()&&tasks.back().duration==0)
-                tasks.back().duration=(time_t)(std::difftime(std::time(nullptr), tasks.back().start));
+            begin(tasks);
 
-            std::cout << "Type task name: ";
-            std::cin >> input;
-            tasks.push_back({input, std::time(nullptr)});
-        }
         else if(input=="end")
-        {
-            if(tasks.back().duration==0)
-                tasks.back().duration=(time_t)(std::difftime(std::time(nullptr), tasks.back().start));
-        }
+            end(tasks);
+
         else if(input=="status")
-        {
-            for(int i=0;i<tasks.size();i++)
-            {
-                std::cout << tasks[i].name << ": ";
-                if(!tasks[i].duration)
-                {
-                    std::cout << (time_t)(std::difftime(std::time(nullptr), tasks.back().start)) << std::endl;
-                }
-                else
-                    std::cout << tasks[i].duration << std::endl;
-            }
-        }
+            status(tasks);
+
     }
     while(input != "exit");
-
 
     return 0;
 }
